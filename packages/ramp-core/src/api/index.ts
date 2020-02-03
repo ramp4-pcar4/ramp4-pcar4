db06faabc586827f4cfd984f27d06389578def6b
Spencer Wahl <spencer.wahl2@canada.ca>
2020-02-03 14:50:39 -0500

    move geoapi loading to api

--- packages/ramp-core/src/api/index.ts
+++ packages/ramp-core/src/api/index.ts
@@ -1,14 +1,30 @@
 import Vue from 'vue';
-
-export * from './internal';
+import GapiLoader, { GeoApi } from 'ramp-geoapi';
 
 import { InstanceAPI } from './internal';
 import mixin from './mixin';
 
+export * from './internal';
+
 // install/register mixin plugin with Vue, so it's available on all Vue instances
 Vue.use(mixin);
 
+interface APIInterface {
+    Instance: typeof InstanceAPI;
+    geoapi: GeoApi;
+    gapiPromise: Promise<GeoApi>;
+}
+
+let api: any = {};
+
+const gapiPromise = GapiLoader(window);
+
+api.Instance = InstanceAPI;
+api.gapiPromise = gapiPromise;
+
+gapiPromise.then((gapi: GeoApi) => {
+    api.geoapi = gapi;
+});
+
 // export `InstanceApi` as `Instance` on global RAMP interface
-export default {
-    Instance: InstanceAPI
-};
+export default api as APIInterface;
