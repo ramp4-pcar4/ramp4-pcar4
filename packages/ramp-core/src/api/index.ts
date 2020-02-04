d8121ebc8ebe27103bc204f02a26f05eab4fcc88
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2020-02-04 06:39:09 -0500

    chore: core setup
    - fix circular import in main-build.ts
    - add mixin plugin to keep an iApi reference on each R4MP Vue instance

--- packages/ramp-core/src/api/index.ts
+++ packages/ramp-core/src/api/index.ts
@@ -1,4 +1,12 @@
+import Vue from 'vue';
+
+export * from './internal';
+
 import { InstanceAPI } from './internal';
+import mixin from './mixin';
+
+// install/register mixin plugin with Vue, so it's available on all Vue instances
+Vue.use(mixin);
 
 // export `InstanceApi` as `Instance` on global RAMP interface
 export default {
