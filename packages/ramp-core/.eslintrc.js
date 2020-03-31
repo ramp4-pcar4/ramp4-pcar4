360c7860b8d978f9854a93090972544ecaafecde
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2020-03-31 13:42:17 -0400

    chore: comment

--- packages/ramp-core/.eslintrc.js
+++ packages/ramp-core/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-dupe-class-members': 'off'
+        'no-dupe-class-members': 'off' // `no-dupe-class-members` complains when function overloads are used
     },
     parserOptions: {
         parser: '@typescript-eslint/parser'
