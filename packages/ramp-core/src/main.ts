8d69eaa44b899ea57ef66517eb3b7a3a0956d94f
Andrew Wang <ay.wang@mail.utoronto.ca>
2020-09-25 10:59:24 -0400

    fix RAMP not loading in cypress

--- packages/ramp-core/src/main.ts
+++ packages/ramp-core/src/main.ts
@@ -1,24 +1,22 @@
-// We have 3 `main` files as Cypress requires a `main.ts`, `main-serve` is used for serves and `main-build` is used for builds.
+// `main.ts` is used for serves and tests, and `main-build.ts` is used for builds
 
-// this file is used when running `rush serve`
+// this file is used when running `rush serve` and `rush test:e2e`
 // when compiled using the `serve` command, the code is treated as an app, not a library,
 // so we need to expose RAMP API on the window manually
 import api from '@/api';
 import Vue from 'vue';
 
-export function startup() {
-    // assign RAMP api to global variable
-    window.RAMP = api;
+// assign RAMP api to global variable
+window.RAMP = api;
 
-    // expose Vue to the global scope so fixtures that use Vue have access to it
-    // this is only needed in `serve` mode; in `build`, Vue is explicitly loaded by the host page
-    // TODO: there are issues with how Vue is loading when using Cypress
-    window.Vue = Vue;
+// expose Vue to the global scope so fixtures that use Vue have access to it
+// this is only needed in `serve` mode; in `build`, Vue is explicitly loaded by the host page
+// TODO: there are issues with how Vue is loading when using Cypress
+window.Vue = Vue;
 
-    // execute `initRAMP` global function if it's defined as soon at the RAMP library is added to the global scope
-    api.gapiPromise.then(() => {
-        if (typeof window.initRAMP === 'function') {
-            window.initRAMP();
-        }
-    });
-}
+// execute `initRAMP` global function if it's defined as soon at the RAMP library is added to the global scope
+api.gapiPromise.then(() => {
+    if (typeof window.initRAMP === 'function') {
+        window.initRAMP();
+    }
+});
