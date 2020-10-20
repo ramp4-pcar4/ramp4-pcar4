62c433cb262352877d4f90dcb5e8e2c58b34c883
Andrew Wang <ay.wang@mail.utoronto.ca>
2020-10-20 10:30:00 -0400

    add feature details zoom button, details translations

--- packages/ramp-core/src/fixtures/details/details-item.vue
+++ packages/ramp-core/src/fixtures/details/details-item.vue
@@ -1,16 +1,24 @@
 <template>
     <panel-screen>
         <template #header>
-            <div class="flex">
-                <span v-html=icon class="flex-initial"> </span>
-                <span class="flex-initial"> {{ itemName }} </span>
-            </div>
+            {{ $t('details.title') }}
         </template>
         <template #controls>
             <back @click="panel.show({ screen: 'details-screen-result', props: { layerIndex: layerIndex } })" v-if="!isFeature"></back>
             <close @click="panel.close()"></close>
         </template>
         <template #content>
+            <div class="flex py-8">
+                <span v-html=icon class="symbologyIcon"> </span>
+                <span class="flex-grow my-auto text-lg"> {{ itemName }} </span>
+                <button @click="zoomToFeature()" class="text-gray-600 m-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20">
+                        <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                        <path d="M0 0h24v24H0V0z" fill="none"/>
+                        <path d="M12 10h-2v2H9v-2H7V9h2V7h1v2h2v1z"/>
+                    </svg>
+                </button>
+            </div>
             <component :is="detailsTemplate" :identifyData="identifyItem"></component>
         </template>
     </panel-screen>
@@ -95,6 +103,25 @@ export default class DetailsItemV extends Vue {
 
         // If nothing is found, use a default template.
         return 'esri-default';
+    }
+
+    zoomToFeature() {
+        const layerInfo = this.payload[this.layerIndex];
+        const uid = layerInfo?.uid || this.uid;
+        const layer: BaseLayer | undefined = this.getLayerByUid(uid);
+        if (layer === undefined) {
+            console.warn(`Could not find layer for uid ${uid} during zoom geometry lookup`);
+            return;
+        }
+        const oid = this.identifyItem.data[layer.getOidField()];
+        const opts = { getGeom: true };
+        layer.getGraphic(oid, opts).then(g => {
+            if (g.geometry === undefined) {
+                console.error(`Could not find graphic for objectid ${oid}`);
+            } else {
+                this.$iApi.map.zoomMapTo(g.geometry, 50000);
+            }
+        });
     }
 }
 </script>
--- packages/ramp-core/src/fixtures/details/index.ts
+++ packages/ramp-core/src/fixtures/details/index.ts
@@ -6,21 +6,25 @@ import DetailsItemV from './details-item.vue';
 import { LayerStore, layer } from '@/store/modules/layer';
 import { IdentifyResult, IdentifyResultSet, IdentifyItem, IdentifyResultFormat, IdentifyParameters, MapClick } from 'ramp-geoapi';
 import BaseLayer from 'ramp-geoapi/dist/layer/BaseLayer';
+import messages from './lang/lang.csv';
 
 class DetailsFixture extends DetailsAPI {
     async added() {
-        this.$iApi.panel.register({
-            'details-panel': {
-                screens: {
-                    'details-screen-layers': DetailsLayerV,
-                    'details-screen-result': DetailsResultV,
-                    'details-screen-item': DetailsItemV
-                },
-                style: {
-                    width: '350px'
+        this.$iApi.panel.register(
+            {
+                'details-panel': {
+                    screens: {
+                        'details-screen-layers': DetailsLayerV,
+                        'details-screen-result': DetailsResultV,
+                        'details-screen-item': DetailsItemV
+                    },
+                    style: {
+                        width: '350px'
+                    }
                 }
-            }
-        });
+            },
+            { i18n: { messages } }
+        );
 
         this.$vApp.$store.registerModule('details', details());
 
