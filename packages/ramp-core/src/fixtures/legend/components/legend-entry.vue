d8aae48412544a20ed26665b0a803a618eb3d71e
Spencer Wahl <spencer.wahl2@canada.ca>
2020-10-22 17:01:20 -0400

    legend UI overhaul for accessibility

--- packages/ramp-core/src/fixtures/legend/components/legend-entry.vue
+++ packages/ramp-core/src/fixtures/legend/components/legend-entry.vue
@@ -1,35 +1,46 @@
 <template>
     <div class="legend-item">
-        <div class="legend-item-header">
-            <!-- symbology stack -->
-            <div @click="toggleSymbology" class="relative">
-                <symbology-stack :visible="displaySymbology" :layer="legendItem.layer" />
-                <tooltip v-if="!displaySymbology" position="right"> {{$t('legend.expand')}} </tooltip>
+        <div
+            class="default-focus-style p-5 flex items-center hover:bg-gray-200 cursor-pointer h-44"
+            @click="$iApi.fixture.get('grid').openGrid(legendItem.layer.uid)"
+            v-focus-item
+        >
+            <!-- symbology stack toggle-->
+            <div class="relative w-32 h-32">
+                <button @click.stop="toggleSymbology">
+                    <symbology-stack class="w-32 h-32" :visible="displaySymbology" :layer="legendItem.layer" />
+                </button>
+                <tooltip v-if="!displaySymbology" position="right"> {{ $t('legend.expand') }} </tooltip>
             </div>
 
             <!-- name -->
-            <div class="flex-1 truncate" @click="$iApi.fixture.get('grid').openGrid(legendItem.layer.uid)">
+            <div class="flex-1 truncate ml-15">
                 <span>{{ legendItem.name }}</span>
             </div>
 
             <!-- visibility -->
             <div>
-                <checkbox :value="visibility" :isRadio="props && props.isVisibilitySet" :legendItem="legendItem"/>
+                <checkbox :value="visibility" :isRadio="props && props.isVisibilitySet" :legendItem="legendItem" />
             </div>
         </div>
 
         <!-- Symbology Stack Section -->
         <div v-if="displaySymbology">
             <!-- display each symbol -->
-            <div class="legend-symbology" v-for="(item, idx) in legendItem.layer.getLegend()" :key="idx">
+            <div
+                class="p-5 flex items-center default-focus-style"
+                v-for="(item, idx) in legendItem.layer.getLegend()"
+                :key="idx"
+                v-focus-item
+            >
                 <div class="symbologyIcon">
                     <span v-html="item.svgcode"></span>
                 </div>
 
-                <div class="flex-1 truncate">{{ item.label }}</div>
+                <div class="flex-1 truncate ml-15">{{ item.label }}</div>
 
                 <!-- TODO: add visibility button functionality. It should toggle each symbol individually. -->
-                <checkbox :value="visibility" :legendItem="legendItem"/>
+                <checkbox :value="visibility" :legendItem="legendItem" />
             </div>
         </div>
     </div>
@@ -61,7 +72,7 @@ export default class LegendEntryV extends Vue {
     mounted(): void {
         this.legendItem.layer?.visibilityChanged.listen((visibility: boolean) => {
             this.visibility = this.legendItem.visibility;
-        })
+        });
     }
 
     /**
@@ -73,14 +84,4 @@ export default class LegendEntryV extends Vue {
 }
 </script>
 
-<style lang="scss" scoped>
-.legend-symbology {
-    @apply px-5 py-5 pr-0 flex items-center;
-}
-.legend-item-header {
-    @apply px-5 py-5 pr-0 flex items-center;
-}
-.legend-item-header:hover {
-    @apply bg-gray-200 cursor-pointer;
-}
-</style>
+<style lang="scss" scoped></style>
