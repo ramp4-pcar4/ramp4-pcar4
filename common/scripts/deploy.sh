060baca387cf8efa9fd17d26a1d90416df85a067
avocadoes <mg_zhu@outlook.com>
2021-02-16 21:02:25 -0500

    sample site

---
+++ common/scripts/deploy.sh
@@ -0,0 +1,21 @@
+# see the following for 'github pages git@github.com: Permission denied (publickey)': 
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
+
+# remove /dist from .gitignore before deploying
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd ../docs/.vuepress/dist
+
+git init
+git add .
+git commit -m 'deploy'
+
+git push -f git@github.com:avocadoes/ramp4-pcar4.git master:gh-pages
+
+cd -
