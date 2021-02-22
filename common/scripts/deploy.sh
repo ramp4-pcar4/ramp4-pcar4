ae94cbd0aad8b51ad2a8691eed9ca81a9702341e
avocadoes <mg_zhu@outlook.com>
2021-02-22 15:06:42 -0500

    remove dist on deploy script

--- common/scripts/deploy.sh
+++ common/scripts/deploy.sh
@@ -1,8 +1,7 @@
 # see the following for 'github pages git@github.com: Permission denied (publickey)': 
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
 
-# after deploying:  delete the common/docs/.vuepress/dist folder
-#                   set the source branch on Settings > Github Pages to gh-pages
+# after deploying: set the source branch on Settings > Github Pages to gh-pages
 
 # abort on errors
 set -e
@@ -19,4 +18,5 @@ git commit -m 'deploy version 1.0.0'
 
 git push -f git@github.com:ramp4-pcar4/ramp4-pcar4.git master:gh-pages
 
-cd -
+cd ..
+rm -rf dist || rmdir /s /q dist
