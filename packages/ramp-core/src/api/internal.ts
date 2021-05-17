093ece590e91dfa6a53f1e1baeaeed4f9ee962d1
Andrew Wang <ay.wang@mail.utoronto.ca>
2021-05-17 14:05:24 -0400

    migrate overview map

--- packages/ramp-core/src/api/internal.ts
+++ packages/ramp-core/src/api/internal.ts
@@ -10,6 +10,7 @@ export * from './instance';
 // that.
 export * from '@/geo/map/basemap';
 export * from '@/geo/map/common-map';
+export * from '@/geo/map/overview-map';
 export * from '@/geo/map/ramp-map';
 export * from '@/geo/layer/file-utils';
 export * from '@/geo/layer/ogc-utils';
