c372be322f76e315ec6b4c407d4b16919fece8a1
Miles Petrov <miles.petrov@canada.ca>
2021-05-17 11:10:33 -0700

    Remove Docker

--- common/scripts/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Some projects have dist dependencies but don't publish builds. 
-# For now we build when the container starts, dist folders will appear shortly after startup.
-cd /home/node/r4mp
-rush build
-
-# Keep container running by replacing current shell with bash interactive
-exec bash
