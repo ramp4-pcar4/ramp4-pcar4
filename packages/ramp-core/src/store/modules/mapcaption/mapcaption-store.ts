9e49ea1fd2040669617ec541e721ca67739f27d1
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2021-05-31 13:54:36 -0400

    migrate map-caption scale event listener to default

--- packages/ramp-core/src/store/modules/mapcaption/mapcaption-store.ts
+++ packages/ramp-core/src/store/modules/mapcaption/mapcaption-store.ts
@@ -7,36 +7,61 @@ import { Attribution } from '@/geo/api';
 
 type MapCaptionContext = ActionContext<MapCaptionState, RootState>;
 
-const getters = {};
+const getters = {
+    getScale: (state: MapCaptionState): any => {
+        return state.scale;
+    }
+};
 
 const actions = {
     setAttribution: (context: MapCaptionContext, attribution: Attribution) => {
         context.commit('SET_ATTRIBUTION', attribution);
+    },
+    setScale: (context: MapCaptionContext, scale: any) => {
+        context.commit('SET_SCALE', scale);
     }
 };
 
 const mutations = {
     SET_ATTRIBUTION: (state: MapCaptionState, value: Attribution) => {
         state.attribution = value;
+    },
+    SET_SCALE: (state: MapCaptionState, value: any) => {
+        state.scale = value;
     }
 };
 
 export enum MapCaptionStore {
     /**
-     * (State) attribution: any // TODO: Create concrete definition type for attribution
+     * (State) attribution: Attribution
      */
     attribution = 'mapcaption/attribution',
     /**
      * (Action) setAttribution: (attribution: any)
      */
-    setAttribution = 'mapcaption/setAttribution!'
+    setAttribution = 'mapcaption/setAttribution!',
+    /**
+     * (State) scale: any
+     */
+    scale = 'mapcaption/scale',
+    /**
+     * (Action) setScale: (scale: any)
+     */
+    setScale = 'mapcaption/setScale!',
+    /**
+     * (Get) getScale: any
+     */
+    getScale = 'mapcaption/getScale!'
 }
 
 export function mapcaption() {
-    const state = new MapCaptionState({
-        text: { disabled: true },
-        logo: { disabled: true }
-    });
+    const state = new MapCaptionState(
+        {
+            text: { disabled: true },
+            logo: { disabled: true }
+        },
+        { label: '0km', width: '0px', isImperialScale: false }
+    );
 
     return {
         namespaced: true,
