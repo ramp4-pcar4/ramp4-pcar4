6aa8165ae19aed85cb333decf4a6127e9a33e5cc
Andrew Wang <ay.wang@mail.utoronto.ca>
2021-07-02 10:54:10 -0400

    put grid buttons into vue components

---
+++ packages/ramp-core/src/fixtures/grid/templates/details-button-renderer.vue
@@ -0,0 +1,45 @@
+<template>
+    <button
+        class="w-38 h-48"
+        :content="`open details`"
+        v-tippy="{ placement: 'right' }"
+        @click="buttonClicked()"
+    >
+        <svg
+            class="m-auto"
+            xmlns="http://www.w3.org/2000/svg"
+            height="16"
+            viewBox="0 0 24 24"
+            width="16"
+        >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+                d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"
+            />
+        </svg>
+    </button>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import deepmerge from 'deepmerge';
+import { GlobalEvents } from '@/api/internal';
+
+@Component
+export default class DetailsButtonRendererV extends Vue {
+    params: any;
+
+    buttonClicked() {
+        const fakeIdentifyItem = deepmerge({}, { data: this.params.data });
+        delete fakeIdentifyItem['data']['rvInteractive'];
+        delete fakeIdentifyItem['data']['rvSymbol'];
+        this.$iApi.event.emit(GlobalEvents.DETAILS_OPEN, {
+            identifyItem: fakeIdentifyItem,
+            uid: this.params.uid
+        });
+    }
+}
+
+</script>
+
+<style lang="scss" scoped></style>
