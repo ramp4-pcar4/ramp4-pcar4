245f1a646c14f77b1d4db4b22169b7289677ae51
Ryan Coulson <ryan.coulson@ec.gc.ca>
2022-04-13 11:24:25 -0400

    geosearch now respects endpoint URLs; add scripts to update types and provinces

--- packages/ramp-core/src/fixtures/geosearch/store/types.ts
+++ packages/ramp-core/src/fixtures/geosearch/store/types.ts
@@ -1,19 +1,42 @@
-import * as jsontypes from './data/types.json';
 import * as defs from './definitions';
+const fetch = require('node-fetch');
 
-const typeObj: { [key: string]: Types } = {};
-const types: defs.GenericObjectType = (<any>jsontypes).default;
+const types: any = {
+    en: {
+        FSA: 'Forward Sortation Area',
+        NTS: 'National Topographic System',
+        COORD: 'Latitude/Longitude',
+        SCALE: 'Scale'
+    },
+    fr: {
+        FSA: "Région De Tri D'Acheminement",
+        NTS: 'Système National De Référence Cartographique',
+        COORD: 'Latitude/Longitude',
+        SCALE: 'Échelle'
+    }
+};
 
 class Types {
     allTypes: defs.GenericObjectType = {};
     validTypes: defs.GenericObjectType = {};
     filterComplete: boolean = false;
 
-    constructor(language: string) {
-        Object.keys(types[language]).forEach(typeKey => {
-            this.allTypes[typeKey] = (<any>types[language])[typeKey];
-            this.validTypes[typeKey] = (<any>types[language])[typeKey];
-        });
+    constructor(language: string, url: string) {
+        const fetchTypes = fetch(url)
+            .then((res: any) => res.json())
+            .then((json: any) => {
+                // Remove the code from the type name.
+                json.definitions.forEach((type: any) => {
+                    types[language][type.code] = type.term.split(
+                        `${type.code}-`
+                    )[1];
+                });
+
+                Object.keys(types[language]).forEach(typeKey => {
+                    this.allTypes[typeKey] = (<any>types[language])[typeKey];
+                    this.validTypes[typeKey] = (<any>types[language])[typeKey];
+                });
+            });
     }
 
     // remove any excluded types indicated by config
@@ -32,8 +55,6 @@ class Types {
     }
 }
 
-export default function (language: string): defs.Types {
-    return (typeObj[language] = typeObj[language]
-        ? typeObj[language]
-        : new Types(language));
+export default function (language: string, url: string): defs.Types {
+    return new Types(language, url);
 }
