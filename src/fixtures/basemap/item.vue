75ec0719affb570914e6126f6403e21822af7451
Reza <Mohsin.Reza@ec.gc.ca>
2022-05-18 16:45:31 -0400

    Fixed issue 1000

--- src/fixtures/basemap/item.vue
+++ src/fixtures/basemap/item.vue
@@ -51,16 +51,26 @@
                 </div>
 
                 <div class="ml-auto pr-5">
-                    <svg
-                        class="fill-current w-16 h-16"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
+                    <a
+                        @click.stop
+                        @keydown.enter.space.prevent
+                        :content="basemap.description"
+                        v-tippy="{
+                            placement: 'bottom',
+                            trigger: 'click focus'
+                        }"
                     >
-                        <path d="M0 0h24v24H0z" fill="none"></path>
-                        <path
-                            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"
-                        />
-                    </svg>
+                        <svg
+                            class="fill-current w-16 h-16"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path
+                                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"
+                            />
+                        </svg>
+                    </a>
                 </div>
             </div>
 
