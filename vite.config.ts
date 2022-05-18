fa049f681c212df99e2273e39fe085e8c7fef169
Miles Petrov <miles.petrov@ec.gc.ca>
2022-05-18 13:22:13 -0400

    build: publish to GitHub pages instead of Azure blob

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import VitePluginI18n from './scripts/vite-plugin-i18n';
 import VitePluginVersion from './scripts/vite-plugin-version';
 import { resolve } from 'path';
 
+const distName = resolve(__dirname, process.env.DIST_NAME || 'dist');
+
 const baseConfig: UserConfigExport = {
     plugins: [vue(), VitePluginI18n(), VitePluginVersion()],
     base: './',
@@ -22,16 +24,18 @@ export default defineConfig(({ command, mode }) => {
     if (command == 'build') {
         if (mode === 'production') {
             baseConfig.build = {
+                outDir: `${distName}`,
                 lib: {
                     entry: resolve(__dirname, 'src/main.ts'),
                     name: 'RAMP',
-                    fileName: format => `ramp.${format}.js`,
-                    formats: ['es', 'umd', 'iife']
+                    fileName: format =>
+                        `ramp.${format == 'iife' ? 'global' : format}.js`,
+                    formats: ['es', 'iife']
                 },
                 rollupOptions: {
                     output: {
                         inlineDynamicImports: true,
-                        dir: 'dist/samples/lib'
+                        dir: `${distName}/lib`
                     }
                 }
             };
@@ -39,7 +43,7 @@ export default defineConfig(({ command, mode }) => {
             baseConfig.publicDir = false;
             baseConfig.root = 'demos';
             baseConfig.build = {
-                outDir: resolve(__dirname, 'dist/demos'),
+                outDir: `${distName}/demos`,
                 rollupOptions: {
                     input: {
                         main: '/index.html'
