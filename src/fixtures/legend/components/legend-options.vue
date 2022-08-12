934a9c1d437f49dbf1bf71a4dc55c733bb4e480e
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2022-08-12 10:58:02 -0400

    remove metadata from default fixtures

--- src/fixtures/legend/components/legend-options.vue
+++ src/fixtures/legend/components/legend-options.vue
@@ -19,27 +19,6 @@
                     </svg>
                 </div>
             </template>
-            <!-- metadata -->
-            <a
-                href="#"
-                class="flex leading-snug items-center text-left w-auto"
-                :class="{
-                    disabled:
-                        !legendItem!.controlAvailable(LayerControls.Metadata) ||
-                        !getFixtureExists('metadata')
-                }"
-                @click="toggleMetadata"
-            >
-                <svg
-                    class="inline-block fill-current w-18 h-18 mr-10"
-                    viewBox="0 0 23 21"
-                >
-                    <path
-                        d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"
-                    />
-                </svg>
-                {{ $t('legend.entry.controls.metadata') }}
-            </a>
             <!-- settings -->
             <a
                 href="#"
