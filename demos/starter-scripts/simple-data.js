a45624d644d282a5dc0caa234cb571d7b7abf3c7
Rory Hofland <roryhofland@gmail.com>
2023-08-15 15:19:54 -0400

    details for data layer

--- demos/starter-scripts/simple-data.js
+++ demos/starter-scripts/simple-data.js
@@ -265,7 +265,12 @@ let config = {
                     name: 'Tasty Eats',
                     layerType: 'data-json',
                     rawData:
-                        '{"fields":["Resto Name","Resto Type","Stars"],"data":[["Grouse Burgers","Burger",5],["Greasy Patties","Burger",2],["Big Dirty Slice","Pizza",3]]}'
+                        '{"fields":["Resto Name","Resto Type","Stars"],"data":[["Grouse Burgers","Burger",5],["Greasy Patties","Burger",2],["Big Dirty Slice","Pizza",3]]}',
+                    fixtures: {
+                        details: {
+                            template: 'Tasty-Template'
+                        }
+                    }
                 },
                 {
                     id: 'table',
@@ -350,6 +355,57 @@ const rInstance = createInstance(
 
 // add export fixtures
 rInstance.fixture.add('export');
+
+rInstance.$element.component('Tasty-Template', {
+    props: ['identifyData'],
+    template: `
+        <div style="align-items: center; justify-content: center; font-size: 14px; font-family: Arial, sans-serif;">
+            <div v-html="createSection('Restaurant', 'Resto-Name')" />
+            <div v-html="createSection('Type', 'Resto-Type')" />
+            <div v-html="createStars('Stars')" />
+        </div>
+    `,
+    methods: {
+        createSection(title, id) {
+            var val = this.identifyData.loaded
+                ? this.identifyData.data[id]
+                : 'Loading...';
+
+            return `
+            <div style="display: flex; flex-direction: column; padding-top: 5px;">
+                <span style="color: #a0aec0; font-weight: bold;">
+                    ${title}
+                </span>
+                <span>
+                    ${val}
+                </span>
+            </div>
+            `;
+        },
+        createStars(n) {
+            var stars =
+                '<img width="24" height="24" src="https://img.icons8.com/material-sharp/24/star--v1.png" alt="star--v1"/>\n'.repeat(
+                    parseInt(this.identifyData.data[n])
+                );
+            var remaining =
+                '<img width="24" height="24" src="https://img.icons8.com/material-sharp/24/b0b0b0/star--v1.png" alt="star--v1"/>'.repeat(
+                    5 - this.identifyData.data[n]
+                );
+
+            return `
+            <div style="display: flex; flex-direction: column; padding-top: 5px;">
+                <span style="color: #a0aec0; font-weight: bold;">
+                    Stars
+                </span>
+                <div style="display: flex; flex-direction: row;">
+                    ${stars}
+                    ${remaining}
+                </div>
+            </div>
+            `;
+        }
+    }
+});
 
 // load map if startRequired is true
 // rInstance.start();
