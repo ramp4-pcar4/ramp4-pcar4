0a1a1ffe76beab2b10f69935adb8a4bdd990c035
Matt Szczerba <szczermw@mcmaster.ca>
2023-11-23 13:22:49 -0500

    Added a toggle to create a flat layer hierarchy

--- src/fixtures/wizard/form-input.vue
+++ src/fixtures/wizard/form-input.vue
@@ -152,6 +152,20 @@
                 </div>
             </div>
         </div>
+        <div v-else-if="type === 'checkbox'">
+            <input
+                class="text-sm border-solid border-gray-300 mb-5 focus:border-green-500 mr-10"
+                type="checkbox"
+                name="nested"
+                :checked="true"
+                @change="
+                    event => {
+                        handleNestedChecked(event);
+                    }
+                "
+            />
+            <label class="text-base font-bold">{{ label }}</label>
+        </div>
         <div v-else>
             <label class="text-base font-bold">{{ label }}</label>
             <div class="relative mb-0.5">
@@ -198,7 +212,8 @@ const emit = defineEmits([
     'link',
     'select',
     'upload',
-    'text'
+    'text',
+    'nested'
 ]);
 
 const props = defineProps({
@@ -336,6 +351,11 @@ const handleServiceSelection = (size: string | number, event: Event) => {
     size
         ? emit('select', (event.target as HTMLInputElement).value)
         : emit('update:modelValue', (event.target as HTMLInputElement).value);
+};
+
+const handleNestedChecked = (event: Event) => {
+    selected.value = [];
+    emit('nested', (event.target as HTMLInputElement).checked);
 };
 
 const handleNameInput = (event: Event) => {
