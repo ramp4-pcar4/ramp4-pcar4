1a2ed95f6285b7b4804ff01ec225e028540bc40c
Ryan Coulson <ryan.coulson@ec.gc.ca>
2024-01-08 09:05:52 -0500

    fix confusing TreeSelect behaviour

---
+++ scripts/vue-slider-component.d.ts
@@ -0,0 +1,4 @@
+// Fix taken from here https://github.com/NightCatSama/vue-slider-component/issues/649#issuecomment-1281910702.
+// When vue-slider-component fixes the type errors we can remove this file.
+
+export { DefineComponent } from 'vue';
