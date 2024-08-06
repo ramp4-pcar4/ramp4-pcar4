b05dca866505c23757e4e615fd480fc7bf62c169
mohsin-r <mohsin.reza@mail.utoronto.ca>
2024-08-06 16:40:29 -0400

    Prevent certain geosearch inputs from displaying bad results

--- src/fixtures/geosearch/search-bar.vue
+++ src/fixtures/geosearch/search-bar.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="rv-geosearch-bar h-26 mb-8 mx-8">
+    <div class="rv-geosearch-bar relative h-26 mx-8 mb-8">
         <input
-            type="search"
+            type="text"
             class="border-b w-full text-base py-8 outline-none focus:shadow-outline border-gray-600 h-full min-w-0"
+            :class="{ 'border-yellow-500': badChars }"
             :placeholder="t('geosearch.searchText')"
             :value="searchVal"
             @input="
@@ -17,6 +18,16 @@
             @keypress.enter.prevent
             enterkeyhint="done"
         />
+        <span class="absolute inset-y-0 right-8 grid w-10 place-content-center">
+            <button
+                v-if="badChars"
+                class="cursor-default"
+                :content="t('geosearch.badChars', { chars: badChars })"
+                v-tippy
+            >
+                ⚠
+            </button>
+        </span>
     </div>
 </template>
 
@@ -32,6 +43,11 @@ const geosearchStore = useGeosearchStore();
 const panelStore = usePanelStore();
 
 const searchVal = computed(() => geosearchStore.searchVal);
+const badChars = computed<string>(() =>
+    ['"', '$', '!', '*', '+', '?', '^', '{', '}', '(', ')', '|', '[', ']']
+        .filter(bc => geosearchStore.searchVal.includes(bc))
+        .join('')
+);
 const setSearchTerm = (value: string) => {
     geosearchStore.setSearchTerm(value);
     geosearchStore.setSearchRegex(value);
