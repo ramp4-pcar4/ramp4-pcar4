a0dae5218dc9bf99cb165c6a1431ea6da1bb99eb
IshavSohal <ishav.sohal@mail.utoronto.ca>
2024-09-11 12:18:56 -0400

    Allow html on result item tooltips, revert to old color picker

--- src/fixtures/details/components/result-item.vue
+++ src/fixtures/details/components/result-item.vue
@@ -22,6 +22,7 @@
                 v-if="data.loaded"
                 class="pl-3 text-left flex-grow itemName"
                 :content="itemName"
+                v-tippy="{ placement: 'right', allowHTML: true }"
                 v-html="makeHtmlLink(itemName)"
                 v-truncate="{
                     options: { placement: 'right-end' }
