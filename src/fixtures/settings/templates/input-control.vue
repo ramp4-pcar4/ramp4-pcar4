ea6516c252ab62e6148d2118e4a2d64c9a3bba72
Miles Petrov <miles.petrov@ec.gc.ca>
2024-11-26 14:02:38 -0500

    vite v6 update and remove esDynamic comments

--- src/fixtures/settings/templates/input-control.vue
+++ src/fixtures/settings/templates/input-control.vue
@@ -21,7 +21,6 @@
 </template>
 
 <script setup lang="ts">
-import config from '@arcgis/core/config';
 import { onBeforeUnmount, reactive, ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
