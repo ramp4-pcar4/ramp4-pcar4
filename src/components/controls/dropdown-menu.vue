a8af2f81253300532dabc4c673ed6840abdf3151
IshavSohal <ishav.sohal@mail.utoronto.ca>
2025-01-08 16:23:30 -0500

    restrict modification of focus list default tab behaviour

--- src/components/controls/dropdown-menu.vue
+++ src/components/controls/dropdown-menu.vue
@@ -106,11 +106,8 @@ onMounted(() => {
     });
 
     dropdownTrigger.value!.addEventListener('focus', focusDropdownTrigger);
-
     dropdownTrigger.value!.addEventListener('blur', blurDropdownTrigger);
-
     dropdownTrigger.value!.addEventListener('mouseover', focusDropdownTrigger);
-
     dropdownTrigger.value!.addEventListener('mouseleave', blurDropdownTrigger);
 
     // nextTick should prevent any race conditions by letting the child elements render before trying to place them using popper
@@ -174,11 +171,8 @@ onBeforeUnmount(() => {
     });
 
     dropdownTrigger.value!.removeEventListener('focus', focusDropdownTrigger);
-
     dropdownTrigger.value!.removeEventListener('blur', blurDropdownTrigger);
-
     dropdownTrigger.value!.removeEventListener('mouseover', focusDropdownTrigger);
-
     dropdownTrigger.value!.removeEventListener('mouseleave', blurDropdownTrigger);
 
     open.value = false;
