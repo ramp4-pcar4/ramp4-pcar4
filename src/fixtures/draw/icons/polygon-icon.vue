4848f329e46ed088106972aefe3f60ea90f517af
Miles Petrov <miles.petrov@ec.gc.ca>
2025-03-18 09:20:39 -0400

    Add initial support for draw tools

---
+++ src/fixtures/draw/icons/polygon-icon.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M3 6l6-3 6 3 6-3v12l-6 3-6-3-6 3z" />
+    </svg>
+</template>
