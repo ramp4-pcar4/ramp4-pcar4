117dc0e17b4ff558a94cff5132e5b27d8cd72028
Spencer Wahl <spencer.s.wahl@gmail.com>
2025-05-20 08:30:42 -0400

    fix freezing on overflowing custom appbar button

--- demos/starter-scripts/panel-party.js
+++ demos/starter-scripts/panel-party.js
@@ -347,13 +347,22 @@ let config = {
                     items: [
                         [
                             'diligord-p1',
+                            'iklob-p1',
+                            'mouruge-p1',
                             {
                                 id: 'gazebo'
-                            },
-                            'iklob-p1',
-                            'mouruge-p1'
+                            }
                         ],
-                        ['snowman', 'legend', 'geosearch', 'basemap']
+                        [
+                            'snowman',
+                            'legend',
+                            'geosearch',
+                            'basemap',
+                            {
+                                id: 'appbar-link',
+                                componentId: 'appbar-link-1'
+                            }
+                        ]
                     ]
                 },
                 mapnav: { items: ['fullscreen', 'help', 'home', 'basemap'] },
@@ -398,6 +407,20 @@ rInstance.$element.component('WFSLayer-Custom', {
             <img src="https://i.imgur.com/WtY0tdC.gif" />
         </div>
     `
+});
+
+rInstance.$element.component(`appbar-link-1`, {
+    template: `<appbar-button :onClickFunction="onClick" tooltip="Custom button">
+        <div class="default fill-current w-24 h-24 ml-8 sm:ml-20">
+            <svg data-slot="icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path clip-rule="evenodd" fill-rule="evenodd" d="M15.75 2.25H21a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V4.81L8.03 17.03a.75.75 0 0 1-1.06-1.06L19.19 3.75h-3.44a.75.75 0 0 1 0-1.5Zm-10.5 4.5a1.5 1.5 0 0 0-1.5 1.5v10.5a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5V10.5a.75.75 0 0 1 1.5 0v8.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V8.25a3 3 0 0 1 3-3h8.25a.75.75 0 0 1 0 1.5H5.25Z"></path>
+            </svg>
+        </div></appbar-button>`,
+    methods: {
+        onClick() {
+            window.open('https://www.google.ca', '_blank').focus();
+        }
+    }
 });
 
 rInstance.$element.component('Water-Quantity-Template', {
