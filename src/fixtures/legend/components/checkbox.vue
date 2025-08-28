d8e7025086d691611d605ed02c3240576a2e2fee
rrjk76 <rrjkong@gmail.com>
2025-08-28 13:32:22 -0400

    api for changing symbol visibility by index

--- src/fixtures/legend/components/checkbox.vue
+++ src/fixtures/legend/components/checkbox.vue
@@ -54,14 +54,6 @@ onMounted(() => {
 });
 
 /**
- * Returns true if none of the child symbols are visible
- * @returns {boolean} true when no child symbols are visible
- */
-const _noSymbolsVisible = (item: LayerItem): boolean => {
-    return !item.symbologyStack.some((item: LegendSymbology) => item.visibility);
-};
-
-/**
  * Toggle this visibility of the value object linked with this checkbox
  */
 const toggleVisibility = (): void => {
@@ -69,42 +61,10 @@ const toggleVisibility = (): void => {
         // Toggle parent symbology checkbox
         props.legendItem.toggleVisibility();
     } else if (props.legendItem instanceof LayerItem) {
-        // Toggle child symbology checkbox
-        if (_noSymbolsVisible(props.legendItem)) {
-            // If no symbols are visible, then set the parent layer to visible
-            // since we toggled on one of the child symbols and set all other
-            // symbols to invisible (except for the one that is toggled on)
-            props.legendItem.setSymbologyVisibility(undefined, false);
-            props.legendItem.setSymbologyVisibility(props.value.uid, true);
-            props.legendItem.toggleVisibility(true);
-        } else {
-            // Toggle the child symbology
-            props.legendItem.setSymbologyVisibility(props.value.uid, !props.value.lastVisbility);
-        }
+        // we clicked a symbol checkbox thats a child of a layer item
+        props.legendItem.clickSymbology(props.value.uid, !props.value.lastVisbility);
 
-        // If all child symbols are toggled off, then toggle off the parent layer too
-        if (_noSymbolsVisible(props.legendItem)) {
-            props.legendItem.toggleVisibility(false);
-        }
-
-        // Update the layer definition to filter child symbols
-        // At the moment, only layers that support features will support sql filters
-        if (props.legendItem.layer?.supportsFeatures) {
-            const filterGuts = props.legendItem.symbologyStack
-                .filter((item: LegendSymbology) => item.lastVisbility === true)
-                .map((item: LegendSymbology) => item.definitionClause || '');
-
-            let sql = ''; // default value, this computes to "show all"
-            if (filterGuts.length === 0) {
-                // nothing visible.
-                sql = '1=2';
-            } else if (filterGuts.length < props.legendItem.symbologyStack.length) {
-                // only a subset of checkboxes are checked. need filter
-                sql = filterGuts.join(' OR ');
-            }
-
-            props.legendItem.layer?.setSqlFilter(CoreFilter.SYMBOL, sql);
-        }
+        // TODO figure out and document what this prop is actually accomplishing and why.
         initialChecked.value = true;
     }
 };
