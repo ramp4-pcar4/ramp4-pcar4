a48d84dfcbd9af4fe87bf978edbd2fe0911cac0c
Michael Waddell <michael@waddellnet.com>
2025-10-21 18:07:56 -0500

    Adding demo for custom language button

---
+++ demos/starter-scripts/custom-lang-button.js
@@ -0,0 +1,96 @@
+import { createInstance, geo } from '@/main';
+
+window.debugInstance = null;
+
+let config = {
+    configs: {
+        en: {
+            map: {
+                extentSets: [
+                    {
+                        id: 'EXT_ESRI_World_AuxMerc_3857',
+                        default: {
+                            xmax: -5007771.626060756,
+                            xmin: -16632697.354854,
+                            ymax: 10015875.184845109,
+                            ymin: 5022907.964742964,
+                            spatialReference: {
+                                wkid: 102100,
+                                latestWkid: 3857
+                            }
+                        }
+                    }
+                ],
+                lodSets: [
+                    {
+                        id: 'LOD_ESRI_World_AuxMerc_3857',
+                        lods: geo.defaultLODs(geo.defaultTileSchemas()[1])
+                    }
+                ],
+                tileSchemas: [
+                    {
+                        id: 'DEFAULT_ESRI_World_AuxMerc_3857',
+                        name: 'Web Mercator Maps',
+                        extentSetId: 'EXT_ESRI_World_AuxMerc_3857',
+                        lodSetId: 'LOD_ESRI_World_AuxMerc_3857',
+                        thumbnailTileUrls: ['/tile/8/91/74', '/tile/8/91/75']
+                    }
+                ],
+                basemaps: [
+                    {
+                        id: 'esriImagery',
+                        tileSchemaId: 'DEFAULT_ESRI_World_AuxMerc_3857',
+                        layers: [
+                            {
+                                layerType: 'esri-tile',
+                                url: 'https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer'
+                            }
+                        ]
+                    }
+                ],
+                initialBasemapId: 'esriImagery'
+            },
+            fixtures: {
+                appbar: {
+                    items: [
+                        {
+                            id: 'lang-switcher',
+                            componentId: 'lang-switcher-button'
+                        }
+                    ]
+                }
+            }
+        }
+    },
+    startingFixtures: ['appbar']
+};
+
+let options = {
+    loadDefaultFixtures: false,
+    loadDefaultEvents: true,
+    startRequired: false
+};
+
+const rInstance = createInstance(document.getElementById('app'), config, options);
+
+window.debugInstance = rInstance;
+
+// create custom appbar button
+rInstance.$element.component('lang-switcher-button', {
+    props: [`options`],
+    template: `<appbar-button :onClickFunction="onClick" tooltip="Click to toggle languagto toggle language!">
+                    <span :style="{ color: color }">{{ language }}</span>
+                </appbar-button>`,
+    data() {
+        return {
+            language: rInstance.language,
+            color: '#BDBDBD'
+        };
+    },
+    methods: {
+        onClick() {
+            const newLang = this.language === 'en' ? 'fr' : 'en';
+            rInstance.setLanguage(newLang);
+        }
+    }
+});
