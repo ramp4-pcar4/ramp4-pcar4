8b0dbf202af2880b519b3ccefe21ffab5fb6d4d6
James Rae <james.b.rae@gmail.com>
2025-11-12 15:00:06 -0500

    swipe fixture config

--- src/fixtures/swipe/index.ts
+++ src/fixtures/swipe/index.ts
@@ -1,8 +1,33 @@
 import SwipeV from './swipe.vue'; // import on-map component
 import { FixtureInstance } from '@/api';
 
+interface SwipeConfig {
+    leading: Array<string>;
+    trailing: Array<string>;
+}
+
 class SwipeFixture extends FixtureInstance {
+    /**
+     * Parses the swipe config snippet from the config json
+     */
+    _parseConfig(swipeConf?: SwipeConfig) {
+        this.leading = swipeConf?.leading ?? [];
+        this.trailing = swipeConf?.trailing ?? [];
+    }
+
+    /**
+     * Layer ids for the left side of the swiper
+     */
+    leading: Array<string> = [];
+
+    /**
+     * Layer ids for the right side of the swiper
+     */
+    trailing: Array<string> = [];
+
     async added(): Promise<void> {
+        this._parseConfig(this.config);
+
         const { el, destroy } = this.mount(SwipeV, {
             app: this.$element,
             props: { fixture: this }
